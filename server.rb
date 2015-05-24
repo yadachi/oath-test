b53c64f77dbbd788e9ea2e1246190d5e46691527
Yosuke Adachi <front.lightstone@gmail.com>
2015-05-24 23:31:10 +0100

    retrieve article

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 require 'net/http'
+require 'json'
 
 configure do 
   enable :sessions
@@ -38,23 +39,29 @@ puts auth_req.body
 auth_res = Net::HTTP.start(auth_uri.hostname, auth_uri.port, :use_ssl => auth_uri.scheme == 'https') do |http|
   http.request(auth_req)
 end
-puts  auth_res.body
-"#{auth_res.body}"
-
-#"#{code}"
-#"<pre>#{res.class.name}</pre>"
-#"<pre>#{res.body}</pre>"
-
-
-#case res
-
-
-#when Net::HTTPSuccess, Net::HTTPRedirection
-#  "<pre>#{res.code}</pre>"
-#  puts res.message
-#  puts res.class.name
-#  puts res.body
-#else
-#  res.value
-#end
+puts  "auth_body #{auth_res.body}"
+auth_result = Hash[URI.decode_www_form(auth_res.body)]
+puts "auth_result: #{auth_result}"
+session[:access_token] = auth_result['access_token']
+puts "session: #{session[:access_token]}"
+"#{session[:access_token]}"
+redirect '/list'
 end
+
+get '/list' do
+retrieve_uri = URI('https://getpocket.com/v3/get')
+retrieve_req = Net::HTTP::Post.new(retrieve_uri, 'Content-Type' => 'application/json')
+retrieve_req.body = {
+  'consumer_key' => '38764-22db1d3236e8c5775086509d',
+  'access_token' => session[:access_token],
+  'state' => 'unread',
+  'contentType' => 'article'
+  }.to_json
+
+retrieve_res = Net::HTTP.start(retrieve_uri.hostname, retrieve_uri.port, :use_ssl => retrieve_uri.scheme == 'https') do |http|
+  http.request(retrieve_req)
+end
+"#{retrieve_res.body}"
+
+end
+
